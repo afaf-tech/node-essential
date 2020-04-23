dca7e2543aa3dd3d1b1293df0c73122d208b5a0e
afaf-tech <fatihalfikri5@gmail.com>
2020-04-23 15:25:39 +0700

    #240000

---
+++ new-poject/routes/hello.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto');
+
+router.get('/time', (req,res)=>{
+    res.json({
+        time: new Date().toISOString()
+    });
+});
+
+router.post('/hash', (req,res,next)=>{
+    const plainText = req.body.plainText;
+    const hash = crypto.createHash('md5').update(plainText).digest('hex');
+    res.json({
+        plainText,
+        hash
+    });
+})
+router.get('/:name', (req,res)=>{
+    // hello?name=james
+    // let name = req.query.name
+
+    // hello/:name
+    const name = req.params.name;
+    res.render('hello', { name});
+});
+
+
+module.exports = router;
