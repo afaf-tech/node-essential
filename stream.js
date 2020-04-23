7ff12fb8b08e11a5a791122308c4817d93f91b24
afaf-tech <fatihalfikri5@gmail.com>
2020-04-23 09:06:30 +0700

    #140000

--- stream.js
+++ stream.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer(
     (req, result) => {
-        const file = fs.createReadStream('./hello.html');
+        const file = fs.createReadStream('./myblog.txt');
         file.pipe(result);
     }
 )
